729bd44035aded39d6fc5977c903fb9c28959f05
chestozo <chestozo@yandex-team.ru>
2013-12-28 15:59:06 +0400

    more tests

--- test/index.js
+++ test/index.js
@@ -9,9 +9,9 @@ describe('Touch', function() {
     describe('2 points info', function() {
         it('0deg',  infoTest([0, 0], [1, 0], { dir: 'right', xdir: 1, ydir: 0, angle: 0, dist: 1 }));
         it('45deg', infoTest([0, 0], [1, 1], { dir: null, xdir: 1, ydir: 1, angle: 45, dist: Math.sqrt(2) }));
-        it('90deg');
-        it('135deg');
-        it('180deg');
+        it('90deg', infoTest([0, 0], [0, 1], { dir: 'top', xdir: 0, ydir: 1, angle: 90, dist: 1 }));
+        it('135deg', infoTest([0, 0], [-1, 1], { dir: null, xdir: -1, ydir: 1, angle: 135, dist: Math.sqrt(2) }));
+        it('180deg', infoTest([0, 0], [-1, 0], { dir: 'left', xdir: -1, ydir: 0, angle: 180, dist: 1 }));
         it('225deg');
         it('270deg');
         it('315deg');
