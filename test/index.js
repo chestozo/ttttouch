9fce7b89388b28b9bac918bcbf7c082d71e1b259
chestozo <chestozo@yandex-team.ru>
2013-12-28 16:07:09 +0400

    one more test and fix info method

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ describe('Touch', function() {
         it('90deg', infoTest([0, 0], [0, 1], { dir: 'top', xdir: 0, ydir: 1, angle: 90, dist: 1 }));
         it('135deg', infoTest([0, 0], [-1, 1], { dir: null, xdir: -1, ydir: 1, angle: 135, dist: Math.sqrt(2) }));
         it('180deg', infoTest([0, 0], [-1, 0], { dir: 'left', xdir: -1, ydir: 0, angle: 180, dist: 1 }));
-        it('225deg');
+        it('225deg', infoTest([0, 0], [-1, -1], { dir: null, xdir: -1, ydir: -1, angle: 225, dist: Math.sqrt(2) }));
         it('270deg');
         it('315deg');
     });
